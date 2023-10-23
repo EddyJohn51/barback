91fb6180947e83f1a4fc8eda4b30808eb1be0450
John Eddy <john.k.eddy.51@gmail.com>
2023-10-23 14:45:19 -0400

    changed names to be consistent with colors in constants/theme.js

--- components/header/screenheader.style.js
+++ components/header/screenheader.style.js
@@ -6,7 +6,7 @@ const styles = StyleSheet.create({
     btnContainer: {
         width: 40,
         height: 40,
-        backgroundColor: COLORS.black,
+        backgroundColor: COLORS.backgroundColor,
         borderRadius: SIZES.small / 1.25,
         justifyContent: 'center',
         alignItems: 'center',
