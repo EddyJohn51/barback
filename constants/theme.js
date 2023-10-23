531a8a4d7f1939a01275847765751da9cf0b02a8
John Eddy <john.k.eddy.51@gmail.com>
2023-10-23 14:42:56 -0400

    added color theme and sizing to constants/theme.js'

---
+++ constants/theme.js
@@ -0,0 +1,20 @@
+const COLORS = {
+    primary: '#EB5E55',
+    secondary: '#0D1821',
+    tertiary: '#2CA58D',
+
+    accent: '#B39C4D',
+
+    backgroundColor: '#E0E1DD;'
+};
+
+const SIZES = {
+    xSmall: 10,
+    small: 12,
+    medium: 16,
+    large: 20,
+    xLarge: 24,
+    xxLarge: 32,
+};
+
+export {COLORS, SIZES};
