86fc3bb4f2e26b39ab76dcd3859c57c2393e082d
John Eddy <john.k.eddy.51@gmail.com>
2023-10-23 15:47:31 -0400

    created file for PopualrRecipes component

---
+++ components/home/popular/PopularRecipes.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import styles from './popularrecipes.style';
+
+const PopularRecipes = () => {
+    return (
+        <View>
+            <Text>Popular Recipes</Text>
+        </View>
+    );
+};
+
+export default PopularRecipes;
