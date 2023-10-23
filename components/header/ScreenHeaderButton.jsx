79ea710a3a1eff6f820f421cd0134588cac7f125
John Eddy <john.k.eddy.51@gmail.com>
2023-10-23 14:35:47 -0400

    created dummy element for ScreenHeaderButtons

---
+++ components/header/ScreenHeaderButton.jsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+
+import styles from './screenheader.style';
+
+const ScreenHeaderButton = () => {
+    return (
+        <View>
+            <Text>ScreenHeaderButton</Text>
+        </View>
+    )
+};
+
+export default ScreenHeaderButton;
