7ae74d0eac6cd0bd79fb2779f15d3a130d16feb8
John Eddy <john.k.eddy.51@gmail.com>
2023-10-23 14:46:14 -0400

    adding components to header on welcome screen

--- app/index.js
+++ app/index.js
@@ -3,10 +3,22 @@ import {View, SafeAreaView, ScrollView} from 'react-native'
 import {Stack, useRouter} from 'expo-router';
 
 const Home = () => {
+    const router = useRouter();
 
     return (
-        <View>
-        </View>
+        <SafeAreaView style={{flex: 1, backgroundColor: '#FFFFFF'}}>
+            <Stack.Screen options={{
+                headerStyle: {backgroundColor: '#FFFFFF'},
+                headerShadowVisible: false,
+                headerLeft: () => {
+                    <ScreenHeaderButton />
+                },
+                headerRight: () => {
+                    <ScreenHeaderButton />
+                },
+                headerTitle: "",
+            }} />
+        </SafeAreaView>
     )
 };
 
