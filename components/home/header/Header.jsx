a9c271838881f2608757512228a28c485c092b18
John Eddy <john.k.eddy.51@gmail.com>
2023-10-23 15:38:38 -0400

    created new files for header component

---
+++ components/home/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, TextInput, TouchableOpacity, View} from 'react-native';
+
+import styles from './header.style';
+
+const Header = () =>  {
+    return (
+        <View>
+            <Text>Header</Text>
+        </View>
+    );
+};
+
+export default Header;
