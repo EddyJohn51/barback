64789d585ed83d9d8f2173d9635a01d2a61d847d
John Eddy <john.k.eddy.51@gmail.com>
2023-10-24 16:19:59 -0400

    added preloading for fonts

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,25 @@
 import {Stack} from 'expo-router';
+import {useCallback} from 'react';
+import {useFonts} from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync()
 
 const Layout = () => {
-    return <Stack />
+    const [fontsLoaded] =useFonts({
+        KanitMedium: require('../assets/fonts/Kanit-Medium.ttf'),
+        KanitExtraLightItalic: require('../assets/fonts/Kanit-ExtraLightItalic.ttf'),
+    })
+
+    const onLayoutRootView = useCallback(async () => {
+        if(fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if(!fontsLoaded) return null;
+
+    return <Stack onLayout={onLayoutRootView}/>
 };
 
 export default Layout;
