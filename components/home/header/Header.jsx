28095b531ee3d40201badae78d0f3b16730ecbea
John Eddy <john.k.eddy.51@gmail.com>
2023-10-24 16:02:52 -0400

    added icons to header component

--- components/home/header/Header.jsx
+++ components/home/header/Header.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
-import {Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {Text, TextInput, TouchableOpacity, View, Image} from 'react-native';
 
 import styles from './header.style';
+import {COLORS, icons} from '../../../constants';
 
 const Header = () =>  {
     return (
         <View>
-            <Text>Header</Text>
+            <View>
+                <Text>BARBACK</Text>
+                <Text>Shake, Stir, Sip: Cocktails at Your Fingertips</Text>
+            </View>
+
+            <View>
+                <TextInput 
+                    style={{backgroundColor: COLORS.primary}}
+                    placeholder='What Ingredient do you want to use?'
+                />
+            </View>
+
+            <View>
+                <TouchableOpacity>
+                    <Image
+                        source={icons.search}
+                        resizeMode='contain'
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Image
+                        source={icons.camera}
+                        resizeMode='contain'
+                    />
+                </TouchableOpacity>
+            </View>
         </View>
+
+
     );
 };
 
