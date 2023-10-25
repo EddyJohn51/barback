382820da6eb4e4eed89bc94cf3bb727236d7ee12
John Eddy <john.k.eddy.51@gmail.com>
2023-10-25 15:05:49 -0400

    rendering placeholder PopularRecipeCards to screen

---
+++ components/cards/PopularRecipeCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import styles from './PopularRecipeCard.style';
+
+const PopularRecipeCard = () => {
+    return (
+        <View>
+            <Text>PopularRecipeCard</Text>
+        </View>
+    );
+}
+
+export default PopularRecipeCard;
--- components/home/popular/PopularRecipes.jsx
+++ components/home/popular/PopularRecipes.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, FlatList, ActivityIndicator} from 'react-native';
+import {View, Text, TouchableOpacity, ActivityIndicator} from 'react-native';
 import {useRouter} from 'expo-router';
 
 
 import styles from './popularrecipes.style';
 import {COLORS, SIZES} from '../../../constants';
-import {PopularRecipeCard} from '../../cards/PopularRecipeCard';
+import PopularRecipeCard from '../../cards/PopularRecipeCard';
+
+const data = [1,2,3,4];
 
 const PopularRecipes = () => {
     return (
         <View>
-            <Text>Popular Recipes</Text>
+            {data?.map((recipe) =>
+                <PopularRecipeCard />
+            )}
         </View>
     );
 };
