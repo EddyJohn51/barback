315bb3d68824a1f748ee869e0ada7cdbdb78a94b
John Eddy <john.k.eddy.51@gmail.com>
2023-10-25 14:42:51 -0400

    added imports for popular recipe component

--- components/home/popular/PopularRecipes.jsx
+++ components/home/popular/PopularRecipes.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity, FlatList, ActivityIndicator} from 'react-native';
+import {useRouter} from 'expo-router';
+
 
 import styles from './popularrecipes.style';
+import {COLORS, SIZES} from '../../../constants';
+import {PopularRecipeCard} from '../../cards/PopularRecipeCard';
 
 const PopularRecipes = () => {
     return (
