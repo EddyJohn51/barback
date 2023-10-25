c4bb2ccadef9501883989c08900cb6489ceba34e
John Eddy <john.k.eddy.51@gmail.com>
2023-10-25 14:37:39 -0400

    new files for popular recipe card component that will be used in popular recipe card component

--- components/index.js
+++ components/index.js
@@ -1,7 +1,9 @@
 import Header from './home/header/Header';
 import PopularRecipes from './home/popular/PopularRecipes';
+import PopularRecipeCard from './cards/PopularRecipeCard';
 
 export {
     Header,
     PopularRecipes,
+    PopularRecipeCard,
 };
