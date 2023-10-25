2207e86bc6f61c675dcb25511ecd9f4824c8cfc4
John Eddy <john.k.eddy.51@gmail.com>
2023-10-25 17:16:19 -0400

    added handler for barcode scan

--- app/scanner/index.js
+++ app/scanner/index.js
@@ -1,11 +1,28 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, SafeAreaView, TouchableOpacity} from 'react-native';
 import {Stack, useRouter} from 'expo-router';
+import {BarCodeScanner} from 'expo-barcode-scanner'
 
 import {COLORS} from '../../constants';
 
 const Scanner = () => {
     const router = useRouter();
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+
+    useEffect(() => {
+        const getBarCodeScannerPermissions = async () => {
+            const {status} = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        };
+
+        getBarCodeScannerPermissions();
+    }, []);
+
+    const handleBarCodeScanned = ({type, data}) => {
+        setScanned(true);
+        alert(`Barcode of type ${type} scanned! Data is ${data}`);
+    };
 
     return (
         <SafeAreaView style={{flex: 1, backgroundColor: COLORS.primary}}>
