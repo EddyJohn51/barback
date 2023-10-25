56c042a7fe3a92e468ffd6b8dacca8c35abec066
John Eddy <john.k.eddy.51@gmail.com>
2023-10-25 15:33:55 -0400

    added text above cards explaining what the section is

--- components/home/popular/PopularRecipes.jsx
+++ components/home/popular/PopularRecipes.jsx
@@ -7,14 +7,20 @@ import styles from './popularrecipes.style';
 import {COLORS, SIZES} from '../../../constants';
 import PopularRecipeCard from '../../cards/PopularRecipeCard';
 
-const data = [1,2,3,4];
+const data = [1,2,3,4, 5, 6, 7, 8, 9];
 
 const PopularRecipes = () => {
     return (
         <View>
-            {data?.map((recipe) =>
-                <PopularRecipeCard />
-            )}
+            <View>
+                <Text>Popular Recipes</Text>
+            </View>
+
+            <View>
+                {data?.map((recipe) =>
+                    <PopularRecipeCard />
+                )}
+            </View>
         </View>
     );
 };
