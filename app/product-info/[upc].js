4294e888fdc8966303c60a463bebb64a2e1f0f8d
John Eddy <john.k.eddy.51@gmail.com>
2023-10-27 17:25:43 -0400

    added stack and functions to handle refreshing

--- app/product-info/[upc].js
+++ app/product-info/[upc].js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useCallback, useState} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, ScrollView, ActivityIndicator, RefreshControl, SafeAreaView} from 'react-native';
 import {Stack, useRouter, useLocalSearchParams} from 'expo-router';
 
 import {COLORS, SIZES} from '../../constants';
@@ -10,13 +10,32 @@ const ProductInfo = () => {
     const router = useRouter();
     const params = useLocalSearchParams();
 
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        refetchProductInfo();
+        setRefreshing(false);
+    }, []);
+
     const {data, isLoading, error, refetchProductInfo} = useFetchProductInfo(params.upc);
 
     return (
-        <View>
-            <Text>Product info for {params.upc}</Text>
-            <Text>{data.total}</Text>  
-        </View>
+        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.primary}}>
+            <Stack.Screen
+            options= {{
+                headerStyle: {backgroundColor: COLORS.primary},
+                headerShadowVisible: false,
+                headerTitle: "",
+            }}
+            />
+
+            <>
+                <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+
+                </ScrollView>
+            </>
+        </SafeAreaView>
     );
 }
 
