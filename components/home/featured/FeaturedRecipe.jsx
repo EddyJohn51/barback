6d75e19bb554e57cd5b0ed3cc2fb87128b54bdeb
John Eddy <john.k.eddy.51@gmail.com>
2023-10-29 15:36:19 -0400

    starting to implement api data in featured drink component

--- components/home/featured/FeaturedRecipe.jsx
+++ components/home/featured/FeaturedRecipe.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useCallback} from 'react';
+import {View, Text, Image} from 'react-native';
+
+import useFetchRecipeInfo from '../../../hook/fetchRecipeInfo'
 
 import styles from './FeaturedRecipe.style';
 
 const FeaturedRecipe = ({recipe}) => {
+    const [refreshing, setRefreshing] = useState(false);
+
+    const {data, isLoading, error, refetchRecipeInfo} = useFetchRecipeInfo('search', 'bloody_mary');
+
     return (
         <View>
             <Text>Featured Recipe: {recipe}</Text>
