d3f8716d22e6a5d6ebb12c871690fe93e33f00ea
John Eddy <john.k.eddy.51@gmail.com>
2023-10-29 16:04:18 -0400

    new component for featured recipe tabs

---
+++ components/tabs/RecipeTabs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import styles from './RecipeTabs.style';
+
+const RecipeTabs = () => {
+    return (
+        <View>
+            <Text>RecipeTab</Text>
+        </View>
+    )
+}
+
+export default RecipeTabs;
