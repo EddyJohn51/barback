b906493d265422b490f8dabb3aee3e6d04575280
John Eddy <john.k.eddy.51@gmail.com>
2023-10-29 16:14:40 -0400

    changed recipes in recipes array

--- app/index.js
+++ app/index.js
@@ -11,7 +11,7 @@ const Home = () => {
     const router = useRouter();
     const {width} = useWindowDimensions();
 
-    const recipes = ['margarita', 'bloody_mary', 'old_fashioned', 'martini'];
+    const recipes = ['daiquiri', 'bloody_mary', 'old_fashioned', 'martini'];
 
     return (
 
