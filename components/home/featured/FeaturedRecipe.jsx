384985b949045aeae3754d08c0ad9aca62848208
John Eddy <john.k.eddy.51@gmail.com>
2023-10-29 15:48:49 -0400

    added image to featured recipe component

--- components/home/featured/FeaturedRecipe.jsx
+++ components/home/featured/FeaturedRecipe.jsx
@@ -1,20 +1,46 @@
 import React, {useState, useCallback} from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, SafeAreaView, ScrollView, RefreshControl, ActivityIndicator} from 'react-native';
 
+import {COLORS, SIZES} from '../../../constants';
 import useFetchRecipeInfo from '../../../hook/fetchRecipeInfo'
 
 import styles from './FeaturedRecipe.style';
 
 const FeaturedRecipe = ({recipe}) => {
     const [refreshing, setRefreshing] = useState(false);
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        refetchRecipeInfo();
+        setRefreshing(false);
+    }, []);
 
-    const {data, isLoading, error, refetchRecipeInfo} = useFetchRecipeInfo('search', 'bloody_mary');
+    const {data, isLoading, error, refetchRecipeInfo} = useFetchRecipeInfo('search', recipe);
 
     return (
-        <View>
-            <Text>Featured Recipe: {recipe}</Text>
-        </View>
-    )
+        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.primary}}>
+            <>
+                <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+                    {isLoading ? (
+                        <ActivityIndicator size='large' color={COLORS.secondary} />
+                    ) : error ? (
+                        <Text>Something Went Wrong!</Text>
+                    ) : data.length === 0 ? (
+                        <Text>No data to render</Text>
+                    ) : (
+                        <View style={{padding: SIZES.medium, paddingBottom: 100}}>
+                            <Image
+                                src={data.drinks[0].strDrinkThumb}
+                                style={{width: 150, height: 150}}
+                                resizeMode='contain'
+                            />
+                            <Text>{data.drinks[0].strDrink}</Text>
+                            {console.log(data.drinks[0])}
+                        </View>
+                    )}
+                </ScrollView>
+            </>
+        </SafeAreaView>
+    );
 }
 
 export default FeaturedRecipe;
