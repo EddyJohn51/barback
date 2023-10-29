038a4f92fc7a0adc17d5b7f6f653ba6aebe5366f
John Eddy <john.k.eddy.51@gmail.com>
2023-10-28 20:27:18 -0400

    added featured recipe component to export list in components directory

--- components/index.js
+++ components/index.js
@@ -1,9 +1,11 @@
 import Header from './home/header/Header';
 import PopularRecipes from './home/popular/PopularRecipes';
 import PopularRecipeCard from './cards/PopularRecipeCard';
+import FeaturedRecipe from './home/featured/FeaturedRecipe';
 
 export {
     Header,
     PopularRecipes,
     PopularRecipeCard,
+    FeaturedRecipe,
 };
