ca468c7a8a72b1a3d45c943ee769d8960132c153
John Eddy <john.k.eddy.51@gmail.com>
2023-11-09 15:10:20 -0500

    created new route for text search

---
+++ app/search/[searchTerm].js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import {ActivityIndicator, FlatList, Image, TouchableOpacity, View, Text, SafeAreaView} from 'react-native';
+import {Stack, useRouter, useLocalSearchParams} from 'expo-router';
+import axios from 'axios';
+
+import {COLORS, SIZES} from '../../constants/';
+//import styles from '../../styles/search';
+
+const RecipeSearch = () => {
+
+}
+
+export default RecipeSearch;
